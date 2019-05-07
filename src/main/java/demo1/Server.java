234ca85894847bdcba36a4f78efbdbbeba5f07b1
geyu <457557667@qq.com>
2019-05-07 20:02:40 +0800

    test

---
+++ src/main/java/demo1/Server.java
@@ -0,0 +1,36 @@
+package demo1;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+
+    private ServerSocket serverSocket;
+
+    public Server(int port){
+        try {
+            this.serverSocket = new ServerSocket(port);
+            System.out.println("服务端启动成功, 端口： " + port);
+        }catch (IOException exception){
+            System.out.println("服务端启动失败");
+        }
+    }
+
+    public void start(){
+        new Thread(new Runnable() {
+            public void run() {
+           //todo
+            }
+        }).start();
+    }
+
+
+    private void doStart(){
+        while (true){
+            Socket client = serverSocket.accept();
+            //new Client
+
+        }
+    }
+}
---
+++ src/main/java/demo1/ServerBoot.java
@@ -0,0 +1,13 @@
+package demo1;
+
+import com.sun.security.ntlm.Server;
+
+public class ServerBoot {
+
+    private static final  int PORT = 8000;
+
+    public static void main(String[] args) {
+
+    }
+
+}
