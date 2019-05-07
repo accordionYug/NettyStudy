c82bae0cc009f5a1dd6408d3e6332c11c842999d
geyu <457557667@qq.com>
2019-05-07 20:12:31 +0800

    demo1 first

--- src/main/java/demo1/ClientHandler.java
+++ src/main/java/demo1/ClientHandler.java
@@ -1,4 +1,41 @@
 package demo1;
 
+import java.io.InputStream;
+import java.net.Socket;
+
 public class ClientHandler {
+
+    public static final int MAX_DATA_LEN = 1024;
+    private final Socket socket;
+
+    public ClientHandler(Socket socket){
+        this.socket = socket;
+    }
+
+    public void start(){
+        System.out.println("新客户端接入");
+        new Thread(new Runnable() {
+            public void run() {
+                doStart();
+            }
+        }).start();
+    }
+
+    public void doStart(){
+        try {
+            InputStream inputStream = socket.getInputStream();
+            while (true){
+                byte[] data = new byte[MAX_DATA_LEN];
+                int len;
+                while ((len = inputStream.read(data))!=-1){
+                    String message = new String(data,0,len);
+                    System.out.println("消息为:"+message);
+                    socket.getOutputStream().write(data);
+                }
+            }
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
 }
